6abffd7aec6803112337e727464d13f6eabc3fd6
lamnt103 <lamnt103@fpt.com>
2025-03-06 23:33:18 +0700

    update cart step1

--- src/pages/Cart/components/Content/Content.jsx
+++ src/pages/Cart/components/Content/Content.jsx
@@ -1,15 +1,16 @@
 import styles from './styles.module.scss';
 import Item from './Item/Item';
+import CartTotal from './CartTotal/CartTotal';
 
 function Content() {
   return (
     <div className={styles.cartContainer}>
-      <div className={styles.cartInfo}>
-        <div></div>
+      <div className={styles.productListCart}>
         <Item />
-        <div></div>
       </div>
-      <div className={styles.totalAndPayment}></div>
+      <div className={styles.totalAndPayment}>
+        <CartTotal />
+      </div>
     </div>
   );
 }
