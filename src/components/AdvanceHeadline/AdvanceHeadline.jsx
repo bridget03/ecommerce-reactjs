252a695ca9d3eeeed245c1ed10b3c6d2ad050fda
bridget03 <nguyenlam2904@gmail.com>
2025-05-31 22:31:10 +0700

    fix responsive homepage

--- src/components/AdvanceHeadline/AdvanceHeadline.jsx
+++ src/components/AdvanceHeadline/AdvanceHeadline.jsx
@@ -1,37 +1,8 @@
 import MainLayout from '@components/Layout/Layout.jsx';
 import styles from './styles.module.scss';
 
-import i18n from 'i18next';
-import { useTranslation, initReactI18next } from 'react-i18next';
-
-const languages = {
-  en: { nativeName: 'English' },
-  vi: { nativeName: 'Tiếng Việt' },
-};
-
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        featureProduct: 'Our featured products',
-      },
-    },
-    vi: {
-      translation: {
-        featureProduct: 'Sản phẩm nổi bật',
-      },
-    },
-  },
-  lng: 'en',
-  fallbackLng: 'en',
-
-  interpolation: {
-    escapeValue: false,
-  },
-});
 function AdvanceHeadline() {
   const { container, headline, containerMiddleBox, des, title } = styles;
-  const { t } = useTranslation();
   return (
     <MainLayout>
       <div className={container}>
